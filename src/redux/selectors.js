d9f29f3b8b31b9d0665dbed3320646ea905eb07c
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-10-19 21:54:33 +0200

    selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,13 +1,36 @@
-// export const getTasks = state => state.tasks;
-
-// export const getStatusFilter = state => state.filters.status;
-
-// export const getTasks = state => state.tasks.items;
-// export const getIsLoading = state => state.tasks.isLoading;
-// export const getError = state => state.tasks.error;
-// export const getStatusFilter = state => state.filters.status;
+import { statusFilters } from './constants';
 
 export const selectTasks = state => state.tasks.items;
 export const selectIsLoading = state => state.tasks.isLoading;
 export const selectError = state => state.tasks.error;
 export const selectStatusFilter = state => state.filters.status;
+
+export const selectVisibleTasks = state => {
+  const tasks = selectTasks(state);
+  const statusFilter = selectStatusFilter(state);
+
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
+
+export const selectTaskCount = state => {
+  const tasks = selectTasks(state);
+
+  return tasks.reduce(
+    (count, task) => {
+      if (task.completed) {
+        count.completed += 1;
+      } else {
+        count.active += 1;
+      }
+      return count;
+    },
+    { active: 0, completed: 0 }
+  );
+};
